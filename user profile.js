76b39481d9dc5b3e9d878e0025b9b21651253742
Juan Paulo Passi <passipaulo14@gmail.com>
2024-11-04 19:35:44 +0800

    Create user profile.js

---
+++ user profile.js
@@ -0,0 +1,10 @@
+let fullName = "Juan Paulo Passi"; 
+let age = 19; 
+let favoriteNumber = 6; 
+let favoriteColor = "Blue"; 
+
+console.log("User Information:");
+console.log("Full Name: " + fullName);
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
